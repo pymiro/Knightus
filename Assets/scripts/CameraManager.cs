e55c777b9dcac6d66ddbdefedc1906a459d6d4af
Paul Lancelot <paul.lancelot59@gmail.com>
2018-05-31 16:09:36 +0200

    camera script

---
+++ Assets/scripts/CameraManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+
+	public GameObject player;
+	private Vector3 playerPos;
+	public Transform playerPlay;
+	public Vector3 zOffset;
+
+	public float forwardOffset;
+
+	public player_move_proto  playerScript;
+
+	public float smoothTime;
+	Vector3 smoothVelocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		CameraMovement ();
+
+	}
+
+	void CameraMovement()
+	{
+		zOffset.x = playerScript.rb.velocity.x;
+
+		playerPos = player.transform.position + zOffset * forwardOffset;
+
+		transform.position = Vector3.SmoothDamp (transform.position, playerPos, ref smoothVelocity, smoothTime);
+	}
+
+}
